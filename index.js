79d830ace9f0d458feb79a8927929fb2c45726b2
Linlin Li <paris1900lll@gmail.com>
2021-05-26 15:38:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const infoSection = document.querySelector(".info-section");
+const listSection = document.querySelector(".list-section");
+const actionSection = document.querySelector(".action-section");
